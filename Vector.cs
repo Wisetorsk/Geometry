8e389e36995b008592341bf1756be8fae47c09e8
Marius Kristiansen <thewisetorsk@gmail.com>
2021-03-20 11:50:58 +0100

    Add project files.

---
+++ Vector.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometry
+{
+    class Vector
+    {
+        public double X { get; set; }
+        public double X0 { get; set; } = 0;
+        public double Y0 { get; set; } = 0;
+        public double Y { get; set; }
+
+        public Point[] points { get; set; }
+
+        public Vector(double x, double y)
+        {
+                
+        }
+
+        public Vector(double x0, double y0)
+        {
+
+        }
+
+        public Vector(Point end)
+        {
+
+        }
+    }
+}
